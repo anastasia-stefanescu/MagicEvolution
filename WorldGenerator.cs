ecc20c1d074b20bff8d742dd48138f5d1d1e2c56
Bagsylina <fabian.anghel.2012@gmail.com>
2024-04-10 19:25:18 +0300

    Functional basic world generation (only 4 basic biomes decided only by altitude)

--- WorldGenerator.cs
+++ WorldGenerator.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic; 
 
 public partial class WorldGenerator : Node2D
 {
@@ -9,7 +10,33 @@ public partial class WorldGenerator : Node2D
 	private double[,] altNoiseMap = new double[height, width];
 	private int[,]  biomeMap = new int[height, width];
 	
-	private double[,] noiseMapGen(float frequency, int octaves)
+	//class that defines the values for a tile to be in a specific biome
+	private class Biome 
+	{ 
+		public int key = 0;
+		public double mintemp = 0, maxtemp = 0, minalt = 0, maxalt = 0;
+		public Biome(int key, double mintemp, double maxtemp, double minalt, double maxalt)
+		{
+			this.key = key;
+			this.mintemp = mintemp;
+			this.maxtemp = maxtemp;
+			this.minalt = minalt;
+			this.maxalt = maxalt;
+		}
+	}
+	Dictionary<String, Biome> BiomeList = new Dictionary<String, Biome>();
+	
+	//function to add in dictionary biomes
+	private void biomeListGen()
+	{
+		BiomeList.Add("Plains", new Biome(0, 0, 100, 45, 70));
+		BiomeList.Add("Ocean", new Biome(1, 0, 100, 0, 40));
+		BiomeList.Add("Beach", new Biome(3, 0, 100, 40, 45));
+		BiomeList.Add("Mountain", new Biome(2, 0, 100, 70, 100));
+	}
+	
+	//function that generates a noise map used to generate values for altitude, temperature, etc.
+	private double[,] noiseMapGen(int height, int width, float frequency, int octaves)
 	{
 		Random random = new Random();
 		var noise = new FastNoiseLite();
@@ -20,16 +47,39 @@ public partial class WorldGenerator : Node2D
 		noise.NoiseType = FastNoiseLite.NoiseTypeEnum.Simplex;
 		double[,] noiseMap = new double[height, width];
 		for(int i = 0; i < height; i++)
-			for(int j = 0; j < height; j++)
-				noiseMap[i, j] = noise.GetNoise2D(i, j) + 0.5;
+			for(int j = 0; j < width; j++)
+				noiseMap[i, j] = (noise.GetNoise2D(i, j) + 1) * 50;
 		return noiseMap;
+	}
+	
+	private void setTile(int x, int y)
+	{
+		foreach(var biome in BiomeList.Values)
+			if(tempNoiseMap[x, y] >= biome.mintemp && tempNoiseMap[x, y] <= biome.maxtemp && altNoiseMap[x, y] >= biome.minalt && altNoiseMap[x, y] <= biome.maxalt)
+			{
+				biomeMap[x, y] = biome.key;
+				GetNode<TileMap>("TileMap").SetCell(0, new Vector2I(x, y), 2, new Vector2I(biome.key, 1), 0);
+				return;
+			}
+		GetNode<TileMap>("TileMap").SetCell(0, new Vector2I(x, y), 2, new Vector2I(0, 1), 0);
+		biomeMap[x, y] = 1;
+	}
+	
+	private void makeMap(int height, int width)
+	{
+		for(int i = 0; i < height; i++)
+			for(int j = 0; j < width; j++)
+				setTile(i, j);
 	}
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		tempNoiseMap = noiseMapGen(0.1f, 5);
-		altNoiseMap = noiseMapGen(0.1f, 5);
+		tempNoiseMap = noiseMapGen(height, width, 0.1f, 5);
+		altNoiseMap = noiseMapGen(height, width, 0.1f, 5);
+		biomeListGen();
+		makeMap(height, width);
+		GD.Print(height);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
