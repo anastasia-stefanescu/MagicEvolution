cbaf7cb91b4adc0a1ea499da83c44bf5e8e45c85
Anastasia Stefanescu <119175350+anastasia-stefanescu@users.noreply.github.com>
2024-05-31 12:26:55 +0300

    mutat calcul in _Process + mici modificari

--- scripts/Wizbit/Wizbit.cs
+++ scripts/Wizbit/Wizbit.cs
@@ -5,11 +5,17 @@ public partial class Wizbit : CharacterBody2D
 {
 	public WizbitStats stats;
 	public NeuralNetwork neuralNetwork;
+	
+	//private int id;
+	//private int id_generator = 0;
+	public Label label;
 
 	private double currentHp;
 	private double currentMana;
-
 	private static readonly double rotation_speed = 2;
+	
+	//cat timp a trecut de la crearea wizbitului
+	private int time_elapsed;
 	
 	//cele vechi, am pus double in loc de float
 	private double ai_x = 0;
@@ -17,14 +23,10 @@ public partial class Wizbit : CharacterBody2D
 	private double ai_rotation = 0;
 	private int frame_cnt = 0;
 
-	//public WizbitStats getWizbitStats()
-	//{
-		//var ref = this.date_wizbit;
-		//return ref;
-	//}
 	
-	
-	public Wizbit() {}
+	public Wizbit() {
+		
+	}
 
 	public override void _EnterTree()
 	{
@@ -37,6 +39,16 @@ public partial class Wizbit : CharacterBody2D
 			neuralNetwork = new NeuralNetwork(GenomeFactory.getStarterNNGenome());
 		AddChild(neuralNetwork);
 	}
+	
+	////ready se apeleaza dupa _EnterTree
+	//public override void _Ready()
+	//{
+		//this.id_generator++;
+		//this.id = this.id_generator;
+		//this.label.Text = "Wizbit " +  this.id; 
+	//}
+
+	
 
 	public Wizbit(WizbitStatsGenome stats, NeuralNetworkGenome nnGenome)
 	{
@@ -77,63 +89,63 @@ public partial class Wizbit : CharacterBody2D
 		neuralNetwork.mutate();
 	}
 	
-	private Vector2 apply_AI_Output()
+	private  AI_Output apply_AI_Output()
 	{
-		//GD.Print("apply Ai output");
 		AI_Output ai_output = neuralNetwork.run(this.construct_AI_input());
 
-		//output: movex, movey, rotate, reproduce
-		
-		Vector2 movement = new Vector2((float)ai_output.moveX, (float)ai_output.moveY).Normalized();
-		movement = movement * (float)stats.getMaxMovementSpeed();
-		
 		if (ai_output.reproduce > 0.5)
 		{
 			this.reproduce();
 		}
 
-		return movement;
+		return ai_output;
 	}
 
 	public override void _Process(double delta)
 	{
-		Vector2 movement = apply_AI_Output();
-		movement = new Vector2(0, 0); // FOR TESTING ONLY
+		//descrestem mana
+		//this.currentMana -= this.stats.base.constantCost * 
+		
+		AI_Output ai_output = apply_AI_Output();
+		
+		Vector2 movement = new Vector2((float)ai_output.moveX, (float)ai_output.moveY) * (float)stats.getMaxMovementSpeed();
 
-		float rot = (float)ai_rotation * (float)rotation_speed * (float)delta;
-		movement = movement.Rotated(rot);
+		float rotation = (float)ai_output.rotate * (float)rotation_speed * (float)delta;
+		movement = movement.Rotated(rotation);
+		
+		//movement = new Vector2(0, 0); // FOR TESTING ONLY
 		Velocity = movement;
 		MoveAndSlide();
+		
 	}
 
 
-	/*public override void _Process(double delta)
-	{
-		Velocity = new Vector2((float)ai_x, (float)ai_y) * (float)stats.getMaxMovementSpeed();
-
-		if (frame_cnt % 20 == 0)
-		{
-			ai_rotation = GD.Randf() * 2 - 1;
-		}
-
-		if (frame_cnt % 60 == 0)
-		{
-			ai_x = GD.Randf() * 2 - 1;
-		}
-
-		if (frame_cnt % 120 == 0)
-		{
-			ai_y = GD.Randf() * 2 - 1;
-		}
-		
-		float rotation = (float)ai_rotation * (float)rotation_speed * (float)delta;
-		Velocity = Velocity.Rotated(rotation);
-
-		frame_cnt = (frame_cnt + 1) % 120;
-		
-		MoveAndSlide();
-	}*/
-	
+	//public override void _Process(double delta)
+	//{
+		//Velocity = new Vector2((float)ai_x, (float)ai_y) * (float)stats.getMaxMovementSpeed();
+//
+		//if (frame_cnt % 20 == 0)
+		//{
+			//ai_rotation = GD.Randf() * 2 - 1;
+		//}
+//
+		//if (frame_cnt % 30 == 0)
+		//{
+			//ai_x = GD.Randf() * 2 - 1;
+		//}
+//
+		//if (frame_cnt % 50 == 0)
+		//{
+			//ai_y = GD.Randf() * 2 - 1;
+		//}
+		//
+		//float rotation = (float)ai_rotation * (float)rotation_speed * (float)delta;
+		//Velocity = Velocity.Rotated(rotation);
+//
+		//frame_cnt = (frame_cnt + 1) % 120;
+		//
+		//MoveAndSlide();
+		//}
 	
 }
 
